756ae1a01b6e81ad171caa41d1612fa98605fceb
= <=>
2018-12-10 14:59:30 +0200

    snapshot before composer update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,9 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            \App\Http\Middleware\Logging::class,
+            'httpHeaders:Come work for US!',
+            \App\Http\Middleware\BasicAuth::class,
         ],
     ];
 
@@ -57,5 +60,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'httpHeaders' => \App\Http\Middleware\HttpHeaders::class,
     ];
 }
